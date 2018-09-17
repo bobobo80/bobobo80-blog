abe17330135aacd4c06948f3c45c7bb0b1f01dd6
bobobo80 <manybobo@gmail.com>
2018-09-17 08:56:52 +0800

    scrapy and pyspider

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,8 @@ TIMEZONE = 'Asia/Shanghai'
 
 DEFAULT_LANG = 'cn'
 
+OUTPUT_PATH = 'docs/'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
